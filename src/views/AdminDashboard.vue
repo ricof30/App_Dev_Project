c6dc04e84222a30432676d2885d01a8b7c3e1bd2
ricof30 <97009967+ricof30@users.noreply.github.com>
2023-11-16 20:29:28 +0800

    added files

---
+++ src/views/AdminDashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container-scroller">
+   <!-- <Sidebar></Sidebar> -->
+   <Header></Header>
+    </div>
+  </template>
+  
+  <script>
+
+  import Header from '../components/AdminHeader.vue';
+      export default {
+        components:{Header},
+          name: 'Main',
+      };
+  </script>
+  
+  <style>
+  
+  </style>
